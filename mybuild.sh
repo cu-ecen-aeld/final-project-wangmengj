21847a785578068330470835c6f8e1bb0cb4ff57
Mengjia Wang <wangmengj@gmail.com>
2024-07-14 00:32:04 -0500

    adding mybuild.sh as well as other files

--- mybuild.sh
+++ mybuild.sh
@@ -2,10 +2,9 @@
 # Script to build image for qemu.
 # Author: Siddhant Jajoo.
 
-
-#git submodule init
-#git submodule sync
-#git submodule update
+git submodule init
+git submodule sync
+git submodule update
 
 # local.conf won't exist until this step on first execution
 source poky/oe-init-build-env
@@ -17,7 +16,15 @@ local_conf_info=$?
 
 if [ $local_conf_info -ne 0 ];then
 	echo "Append ${CONFLINE} in the local.conf file"
-	echo ${CONFLINE} >> conf/local.conf
+
+cat <<EOT >> conf/local.conf
+MACHINE ??= "raspberrypi5"
+LICENSE_FLAGS_ACCEPTED = "synaptics-killswitch"
+MACHINE_FEATURES:append= " vc4graphics "
+DISTRO_FEATURES:append = " directfb directfb-examples kmsdrm opengl gles2 drm-gl alsa"
+CORE_IMAGE_EXTRA_INSTALL += "glmark2 kmscube "
+IMAGE_INSTALL:append = " strace packagegroup-core-buildessential libgl-mesa-dev libsdl2-dev libsdl2-image-dev "
+EOT
 	
 else
 	echo "${CONFLINE} already exists in the local.conf file"
@@ -35,7 +42,9 @@ fi
 #fi
 
 bitbake-layers add-layer ../meta-raspberrypi
+bitbake-layers add-layer ../meta-openembedded/meta-oe
+bitbake-layers add-layer ../meta-aesd 
 bitbake-layers show-layers
 
 set -e
-bitbake core-image-minimal
+bitbake core-image-aesd
